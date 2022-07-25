9847b77fb4033f045c653375561adde7550e020a
ELMIR.WEB <elmir.web@gmail.com>
2022-07-25 15:36:57 +0500

    начат изменятор транспорта

--- react-client/src/COMPONENTS/ACCOUNT/DASHBOARD/VEHICLES/Vehicles.Component.jsx
+++ react-client/src/COMPONENTS/ACCOUNT/DASHBOARD/VEHICLES/Vehicles.Component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { RiDeleteBin4Line } from 'react-icons/ri';
+import { MdOutlineCreate } from 'react-icons/md';
 
 import './Vehicles.Component.scss';
 
@@ -20,7 +22,64 @@ const Vehicles = ({
         создать тип ГСМ
       </button>
 
-      <div>Vehicles</div>
+      <div className='table-wrapper'>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Модель</th>
+              <th>Гос.номер</th>
+              <th>Автомобильная база</th>
+              <th>Тип ГСМ</th>
+              <th>Пробег</th>
+              <th>Топливо</th>
+              <th>Расход</th>
+              <th>Действие</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allVehicles.length ? (
+              allVehicles.map((vehicle) => {
+                return (
+                  <tr key={vehicle.ID}>
+                    <td>{vehicle.ID}</td>
+                    <td>{vehicle.Model}</td>
+                    <td>{vehicle.Number}</td>
+                    <td>{vehicle.IDautobase.Name}</td>
+                    <td>{vehicle.IDtypegsm.Name}</td>
+                    <td>{vehicle.Kilometrs}</td>
+                    <td>{vehicle.Liters}</td>
+                    <td>{vehicle.Expense}</td>
+                    <td className='table-buttons'>
+                      <button
+                        className='table-button beautiful-button-blue'
+                        onClick={() => {
+                          setStatusMountChangeVehicle(vehicle);
+                        }}
+                      >
+                        <MdOutlineCreate />
+                      </button>
+
+                      <button
+                        className='table-button beautiful-button-red'
+                        onClick={() => {
+                          setStatusMountRemoveVehicle(vehicle);
+                        }}
+                      >
+                        <RiDeleteBin4Line />
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan='8'>Транспорт не найден</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
