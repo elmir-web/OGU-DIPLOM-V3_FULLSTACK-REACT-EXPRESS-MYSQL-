3700e81b20bf52014c26b0131310b4c1189efd73
ELMIR.WEB <elmir.web@gmail.com>
2022-07-25 16:06:21 +0500

    удалятор авто в процессе

--- react-client/src/COMPONENTS/ACCOUNT/DASHBOARD/TYPESGSM/REMOVETYPEGSM/RemoveTypeGSM.Component.jsx
+++ react-client/src/COMPONENTS/ACCOUNT/DASHBOARD/TYPESGSM/REMOVETYPEGSM/RemoveTypeGSM.Component.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import "./RemoveTypeGSM.Component.scss";
+import './RemoveTypeGSM.Component.scss';
 
-import LoaderSpinerComponent from "./../../../../LOADERSPINER/LoaderSpiner.Component";
+import LoaderSpinerComponent from './../../../../LOADERSPINER/LoaderSpiner.Component';
 
-import RemoveTypeGSMController from "./RemoveTypeGSM.Controller";
+import RemoveTypeGSMController from './RemoveTypeGSM.Controller';
 
 const RemoveTypeGSM = ({
   dashboardComponentMount,
@@ -14,19 +14,19 @@ const RemoveTypeGSM = ({
   const [loadSpinerActive, setLoadSpinerActive] = useState(false);
 
   return (
-    <div className="RemoveTypeGSM modal-window">
-      {loadSpinerActive === true ? <LoaderSpinerComponent /> : ""}
+    <div className='RemoveTypeGSM modal-window'>
+      {loadSpinerActive === true ? <LoaderSpinerComponent /> : ''}
 
-      <div className="modal-window__popup-form">
-        <header className="modal-window__header">
-          <div className="modal-window__title">
+      <div className='modal-window__popup-form'>
+        <header className='modal-window__header'>
+          <div className='modal-window__title'>
             <span>
               Удаление автомобильной базы ID: {statusMountRemoveTypeGSM.ID}
             </span>
           </div>
 
           <button
-            className="modal-window__button-close"
+            className='modal-window__button-close'
             onClick={() => {
               setStatusMountRemoveTypeGSM(null);
             }}
@@ -35,19 +35,19 @@ const RemoveTypeGSM = ({
           </button>
         </header>
 
-        <main className="modal-window__main">
-          <div className="modal-window__question">
-            <p className="modal-window__question-text">
+        <main className='modal-window__main'>
+          <div className='modal-window__question'>
+            <p className='modal-window__question-text'>
               Вы действительно хотите удалить тип ГСМ?
             </p>
-            <p className="modal-window__question-data">
+            <p className='modal-window__question-data'>
               Название: {statusMountRemoveTypeGSM.Name}
             </p>
           </div>
 
-          <div className="remove-object-buttons">
+          <div className='remove-object-buttons'>
             <button
-              className="remove-object-buttons__no beautiful-button-green"
+              className='remove-object-buttons__no beautiful-button-green'
               onClick={() => {
                 setStatusMountRemoveTypeGSM(null);
               }}
@@ -56,7 +56,7 @@ const RemoveTypeGSM = ({
             </button>
 
             <button
-              className="remove-object-buttons__yes beautiful-button-red"
+              className='remove-object-buttons__yes beautiful-button-red'
               onClick={() => {
                 RemoveTypeGSMController(
                   dashboardComponentMount,
--- react-client/src/COMPONENTS/ACCOUNT/DASHBOARD/VEHICLES/REMOVEVEHICLE/RemoveVehicle.Component.jsx
+++ react-client/src/COMPONENTS/ACCOUNT/DASHBOARD/VEHICLES/REMOVEVEHICLE/RemoveVehicle.Component.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './RemoveVehicle.Component.scss';
+
+import LoaderSpinerComponent from './../../../../LOADERSPINER/LoaderSpiner.Component';
 
 import RemoveVehicleController from './RemoveVehicle.Controller';
 
@@ -9,7 +11,37 @@ const RemoveVehicle = ({
   statusMountRemoveVehicle,
   setStatusMountRemoveVehicle,
 }) => {
-  return <div className='RemoveVehicle'>RemoveVehicle</div>;
+  const [loadSpinerActive, setLoadSpinerActive] = useState(false);
+
+  return (
+    <div className='RemoveVehicle modal-window'>
+      {loadSpinerActive === true ? <LoaderSpinerComponent /> : ''}
+
+      <div className='modal-window__popup-form'>
+        <header className='modal-window__header'>
+          <div className='modal-window__title'>
+            <span>Удаление транспорта ID: {statusMountRemoveVehicle.ID}</span>
+          </div>
+          <button
+            className='modal-window__button-close'
+            onClick={() => {
+              setStatusMountRemoveVehicle(null);
+            }}
+          >
+            X
+          </button>
+        </header>
+
+        <main className='modal-window__main'>
+          <div className='modal-window__question'>
+            <p className='modal-window__question-text'>
+              Вы действительно хотите удалить транспорт?
+            </p>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default RemoveVehicle;
